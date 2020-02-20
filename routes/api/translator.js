ed3815c3edc9452fd58f57e6daf10dcfa0e39519
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-20 15:50:10 -0600

    server setup

---
+++ routes/api/translator.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const router = express.Router();
+
+// @route POST api/users
+// @description Register user
+// @access Public
+router.post("/", async (req, res) => {});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use("/api/translator", require("./routes/api/translator"));
