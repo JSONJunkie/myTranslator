d7cd3f16b12d4bceac63a9c3facbfdf8016ee472
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-20 13:55:09 -0600

    Store and Root reducer setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+import rootReducer from "./reducers";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
