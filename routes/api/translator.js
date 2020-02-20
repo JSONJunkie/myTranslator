ea2292c6b19a00f5eaf58b52587558ed38a7724b
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-20 17:27:39 -0600

    setup and tested translator

--- routes/api/translator.js
+++ routes/api/translator.js
@@ -6,10 +6,27 @@ const router = express.Router();
 const languageTranslator = new LanguageTranslatorV3({
   version: "2018-05-01",
   authenticator: new IamAuthenticator({
-    apikey: "process.env.API_KEY"
+    apikey: process.env.API_KEY
   }),
-  url: "process.env.API_URL"
+  url: process.env.API_URL,
+  headers: {
+    "X-Watson-Learning-Opt-Out": "true"
+  }
 });
+
+const translateParams = {
+  text: "Hello",
+  modelId: "en-es"
+};
+
+languageTranslator
+  .translate(translateParams)
+  .then(translationResult => {
+    console.log(JSON.stringify(translationResult, null, 2));
+  })
+  .catch(err => {
+    console.log("error:", err);
+  });
 
 // @route POST api/users
 // @description Register user
