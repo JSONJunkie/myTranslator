7b9c224c5e43ad5d22517bfd093952ed8eafc560
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-20 16:40:41 -0600

    hide api key and url in env and set up translator in its route

--- routes/api/translator.js
+++ routes/api/translator.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const LanguageTranslatorV3 = require("ibm-watson/language-translator/v3");
+const { IamAuthenticator } = require("ibm-watson/auth");
 
 const router = express.Router();
+const languageTranslator = new LanguageTranslatorV3({
+  version: "2018-05-01",
+  authenticator: new IamAuthenticator({
+    apikey: "process.env.API_KEY"
+  }),
+  url: "process.env.API_URL"
+});
 
 // @route POST api/users
 // @description Register user
