15cd9b76e4e69716e1bd149a22ac547e254359d0
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-22 15:42:47 -0600

    no success yet in safari, even using callbacks. the failure call back function of decodeAudioData is firing with no error to be found

--- client/src/actions/lang.js
+++ client/src/actions/lang.js
@@ -38,13 +38,69 @@ export const speak = async postTrans => {
     const audio = res.data;
     const AudioContext = window.AudioContext || window.webkitAudioContext;
     const audioCtx = new AudioContext();
-    const source = audioCtx.createBufferSource();
-    const decodedData = await audioCtx.decodeAudioData(audio);
-    source.buffer = decodedData;
+    // const oscillator = audioCtx.createOscillator();
+    // oscillator.frequency.value = 400;
+    // oscillator.connect(audioCtx.destination);
+    // oscillator.start(0);
+    // oscillator.stop(0.5);
+    // const decodedData = await audioCtx.decodeAudioData(audio);
+    // source.buffer = decodedData;
+    // function bufferSound(audio) {
+    //   var mySource;
 
-    source.connect(audioCtx.destination);
-    source.start();
-    console.log("hello");
+    //   var source = audioCtx.createBufferSource();
+    //   source.buffer = audioCtx.createBuffer(audio, false);
+    //   mySource = source;
+    //   mySource.start();
+    // }
+    // let audioBuffer;
+    // console.log(audio);
+    var playSoundBuffer;
+    const bufferSound = audio => {
+      console.log(audio);
+
+      audioCtx.decodeAudioData(
+        audio,
+        buffer => {
+          playSoundBuffer = buffer;
+          playSound();
+
+          // console.log("hello");
+          // const source = audioCtx.createBufferSource();
+          // source.buffer = buffer;
+          // source.connect(audioCtx.destination);
+          // source.start(0);
+        },
+        function(err) {
+          console.log("hell");
+          if (err) console.log("Error with decoding audio data" + err);
+        }
+      );
+
+      console.log(audio);
+    };
+    function playSound() {
+      console.log(playSoundBuffer);
+      var source = audioCtx.createBufferSource();
+      source.buffer = playSoundBuffer; // This is the line that generates the error
+      source.connect(audioCtx.destination);
+      source.start(0);
+    }
+    bufferSound(audio);
+
+    // audioCtx.decodeAudioData(
+    //   audio,
+    //   buffer => {
+    //     audioBuffer = buffer;
+    //     console.log(audioBuffer);
+    //     source.buffer = audioBuffer;
+    //     source.connect(audioCtx.destination);
+    //   },
+
+    //   function(err) {
+    //     if (err) console.log("Error with decoding audio data" + err);
+    //   }
+    // );
   } catch (err) {
     console.log(err);
   }
