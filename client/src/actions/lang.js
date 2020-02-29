33b56d63051d8b50c05a215285046602b5cbdda5
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-02-29 04:17:17 -0600

    brought logic back into actions file

--- client/src/actions/lang.js
+++ client/src/actions/lang.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 import { TRANSLATE, SPEAK, LISTEN } from "./types";
 import playSound from "../utils/playSound";
-import recordAudio from "../utils/recordAudio";
 
 export const translate = formData => async dispatch => {
   try {
@@ -54,6 +53,44 @@ export const speak = postTrans => async dispatch => {
 
 export const listen = () => async dispatch => {
   try {
+    const constraints = { audio: true };
+
+    let stream = null;
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = function(constraints) {
+        const getUserMedia =
+          navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        if (!getUserMedia) {
+          return Promise.reject(new Error("This browser is not supported"));
+        }
+
+        return new Promise(function(resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      };
+    }
+    setTimeout(() => {
+      mediaRecorder.stop();
+      console.log("recording stopping");
+    }, 4000);
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    const mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start(500);
+    console.log("recording starting");
+    let chunks = [];
+    let audio = [];
+
+    mediaRecorder.ondataavailable = function(e) {
+      chunks.push(e.data);
+      console.log("chunk collected");
+    };
+
+    mediaRecorder.onstop = function(e) {
+      const blob = new Blob(chunks, { type: "audio/webm" });
+      console.log("recording stopping");
+      chunks = [];
+    };
     // const config = {
     //   headers: {
     //     "Content-Type": "blob.type"
