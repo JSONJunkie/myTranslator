6bd5af7c06ad4b70a200ff8133df9b800325c5d4
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-03-13 18:08:43 -0500

    material ui textfields

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,21 +1,40 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
 
 import { translate, speak, listen } from "../../actions/lang";
 import legacyGetUserMedia from "../../utils/legacyRecording";
 
-const textArea = {
-  width: "60%",
-  height: "20%"
-};
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    height: "100vh"
+  },
+  content: {
+    flexGrow: 1,
+    display: "flex",
+    flexDirection: "column",
+    marginTop: theme.spacing(8),
+    overflow: "auto"
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1)
+  }
+}));
 
 const Landing = ({
   translate,
   speak,
   listen,
-  lang: { postTrans, transcribed }
+  lang: { postTrans, transcribed, translatedTranscription }
 }) => {
+  const classes = useStyles();
+
   let [stream, setStream] = useState(null);
   const [mediaRecorder, setMediaRecorder] = useState(null);
   const [text, setText] = useState("");
@@ -37,7 +56,7 @@ const Landing = ({
           setMediaRecorder(new MediaRecorder(stream));
         }
         if (navigator.mediaDevices.getUserMedia) {
-          setSupported(!supported);
+          setSupported(true);
         }
       } catch (err) {
         console.log(err);
@@ -92,31 +111,63 @@ const Landing = ({
   };
 
   return supported ? (
-    <Fragment>
-      Welcome to the translator! To begin, enter text below:
-      <textarea
-        placeholder={"Enter text here..."}
-        style={textArea}
-        value={text}
-        onChange={e => onChange(e)}
-      />
-      <button onClick={e => handleClick(e)}>Translate</button>
-      <textarea
-        placeholder={"Translated text will appear here..."}
-        style={textArea}
-        value={postTrans}
-        readOnly
-      />
-      <button onClick={e => handleClick2(e)}>Speak!</button>
-      <textarea
-        placeholder={"Transcribed text will appear here..."}
-        style={textArea}
-        value={transcribed}
-        readOnly
-      />
-      {!listening && <button onClick={e => handleClick3(e)}>Listen!</button>}
-      {listening && <button onClick={e => handleClick3(e)}>Stop!</button>}
-    </Fragment>
+    <div className={classes.root}>
+      <Container className={classes.content}>
+        Welcome to the translator! To begin, enter text below:
+        <Paper>
+          <form className={classes.form}>
+            <TextField
+              aria-label="untranslated text"
+              name="text"
+              value={text}
+              variant="filled"
+              placeholder="Enter text to be translated here..."
+              fullWidth
+              multiline
+              rows={4}
+              onChange={e => onChange(e)}
+            />
+            <button onClick={e => handleClick(e)}>Translate</button>
+            <TextField
+              aria-label="translated text"
+              name="postTrans"
+              value={postTrans}
+              variant="filled"
+              placeholder="Translated text will appear here..."
+              fullWidth
+              multiline
+              rows={4}
+              inputProps={{ disabled: true }}
+            />
+            <button onClick={e => handleClick2(e)}>Speak!</button>
+            <TextField
+              aria-label="transcribed text"
+              value={transcribed}
+              variant={"filled"}
+              placeholder="Transcribed text will appear here..."
+              fullWidth
+              multiline
+              rows={4}
+              inputProps={{ disabled: true }}
+            />
+            <TextField
+              aria-label="translated transcribed text"
+              value={translatedTranscription}
+              variant={"filled"}
+              placeholder="Translated transcription will appear here..."
+              fullWidth
+              multiline
+              rows={4}
+              inputProps={{ disabled: true }}
+            />
+            {!listening && (
+              <button onClick={e => handleClick3(e)}>Listen!</button>
+            )}
+            {listening && <button onClick={e => handleClick3(e)}>Stop!</button>}
+          </form>
+        </Paper>
+      </Container>
+    </div>
   ) : (
     <Fragment>Browser not supported</Fragment>
   );
