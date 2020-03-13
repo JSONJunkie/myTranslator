d635789d83c11c6d731665161611031684a11651
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-03-13 16:22:48 -0500

    prevent api from being used so char limits wont be reached

--- client/src/actions/lang.js
+++ client/src/actions/lang.js
@@ -4,82 +4,72 @@ import { TRANSLATE, SPEAK, LISTEN } from "./types";
 import playSound from "../utils/playSound";
 
 export const translate = formData => async dispatch => {
-  try {
-    const translateParams = {
-      text: formData,
-      modelId: "en-es"
-    };
-    const body = translateParams;
-
-    const res = await axios.post("/api/translator", body);
-
-    dispatch({
-      type: TRANSLATE,
-      payload: { preTrans: formData, postTrans: res.data }
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  // try {
+  //   const translateParams = {
+  //     text: formData,
+  //     modelId: "en-es"
+  //   };
+  //   const body = translateParams;
+  //   const res = await axios.post("/api/translator", body);
+  //   dispatch({
+  //     type: TRANSLATE,
+  //     payload: { preTrans: formData, postTrans: res.data }
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 };
 
 export const speak = postTrans => async dispatch => {
-  try {
-    const synthesizeParams = {
-      text: postTrans,
-      accept: "audio/mp3",
-      voice: "es-ES_LauraVoice"
-    };
-
-    const config = {
-      responseType: "arraybuffer"
-    };
-
-    const body = synthesizeParams;
-
-    const res = await axios.post("/api/translator/speak", body, config);
-
-    const audio = res.data;
-
-    playSound(audio);
-
-    dispatch({
-      type: SPEAK,
-      payload: { msg: "Playing back translation" }
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  // try {
+  //   const synthesizeParams = {
+  //     text: postTrans,
+  //     accept: "audio/mp3",
+  //     voice: "es-ES_LauraVoice"
+  //   };
+  //   const config = {
+  //     responseType: "arraybuffer"
+  //   };
+  //   const body = synthesizeParams;
+  //   const res = await axios.post("/api/translator/speak", body, config);
+  //   const audio = res.data;
+  //   playSound(audio);
+  //   dispatch({
+  //     type: SPEAK,
+  //     payload: { msg: "Playing back translation" }
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 };
 
 export const listen = blob => async dispatch => {
-  try {
-    const config = {
-      headers: {
-        "Content-Type": "blob.type"
-      }
-    };
-    const transcribedRes = await axios.post(
-      "/api/translator/listen",
-      blob,
-      config
-    );
-    dispatch({
-      type: LISTEN,
-      payload: { transcribed: transcribedRes.data }
-    });
-    const translateParams = {
-      text: transcribedRes.data,
-      modelId: "es-en"
-    };
-    const body = translateParams;
-
-    const translatedRes = await axios.post("/api/translator", body);
-
-    dispatch({
-      type: TRANSLATE,
-      payload: { preTrans: transcribedRes.data, postTrans: translatedRes.data }
-    });
-  } catch (err) {
-    console.log(err);
-  }
+  // try {
+  //   const config = {
+  //     headers: {
+  //       "Content-Type": "blob.type"
+  //     }
+  //   };
+  //   const transcribedRes = await axios.post(
+  //     "/api/translator/listen",
+  //     blob,
+  //     config
+  //   );
+  //   dispatch({
+  //     type: LISTEN,
+  //     payload: { transcribed: transcribedRes.data }
+  //   });
+  //   const translateParams = {
+  //     text: transcribedRes.data,
+  //     modelId: "es-en"
+  //   };
+  //   const body = translateParams;
+  //   const translatedRes = await axios.post("/api/translator", body);
+  //   dispatch({
+  //     type: TRANSLATE,
+  //     payload: { preTrans: transcribedRes.data, postTrans: translatedRes.data }
+  //   });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 };
