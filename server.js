678197e5bbfbdab58dac226d2a13ea6b4fd11fb2
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-04-10 23:00:13 -0500

    Prep for deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -10,3 +11,14 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use("/api/translator", require("./routes/api/translator"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, "client", "build")));
+
+  // Serve index.html on all routes except the api routes above
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
