36a5207202420b3d3356bc93ffa27dd631916bcf
Deleon Reescano <deleon.reescano@ricealumni.net>
2020-04-12 14:24:40 -0500

    audio context fix for safari

--- client/src/utils/playSound.js
+++ client/src/utils/playSound.js
@@ -1,6 +1,6 @@
 export default function playSound(audio) {
-  const AudioContext = window.AudioContext || window.webkitAudioContext;
-  const audioCtx = new AudioContext();
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  // const audioCtx = new AudioContext();
   const buffer = audioCtx.createBuffer(1, 1, 22050);
   const source = audioCtx.createBufferSource();
   source.buffer = buffer;
@@ -26,7 +26,7 @@ export default function playSound(audio) {
         source.noteOn(0);
       }
     },
-    function(err) {
+    function (err) {
       console.log("Error with decoding audio data" + err);
     }
   );
