6d9e6ddc0f1255a45baa94ff7a7a997b7e0bfd6c
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-04-21 19:49:59 -0500

    still not getting desired traceback for prod or dev, used to get desired stacetrack in prod when generating rollbar object inside functional component so doing that again

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,25 +3,25 @@ import Rollbar from "rollbar";
 
 import Landing from "./landing/Landing";
 
-function getRollbar() {
-  if (process.env.NODE_ENV === "development") {
-    const rollbar = new Rollbar({
-      accessToken: "589f75cdf3664555b9b778a76ab2a226",
-      captureUncaught: true,
-      captureUnhandledRejections: true
-    });
-    return rollbar;
-  }
+// function getRollbar() {
+//   if (process.env.NODE_ENV === "development") {
+//     const rollbar = new Rollbar({
+//       accessToken: "589f75cdf3664555b9b778a76ab2a226",
+//       captureUncaught: true,
+//       captureUnhandledRejections: true
+//     });
+//     return rollbar;
+//   }
 
-  if (process.env.NODE_ENV === "production") {
-    const rollbar = new Rollbar({
-      accessToken: "589f75cdf3664555b9b778a76ab2a226",
-      captureUncaught: true,
-      captureUnhandledRejections: true
-    });
-    return rollbar;
-  }
-}
+//   if (process.env.NODE_ENV === "production") {
+//     const rollbar = new Rollbar({
+//       accessToken: "589f75cdf3664555b9b778a76ab2a226",
+//       captureUncaught: true,
+//       captureUnhandledRejections: true
+//     });
+//     return rollbar;
+//   }
+// }
 
 const flexContainer = {
   display: "flex",
@@ -32,7 +32,13 @@ const flexContainer = {
 };
 
 function App() {
-  const [rollbar] = useState(getRollbar());
+  const [rollbar] = useState(
+    new Rollbar({
+      accessToken: "589f75cdf3664555b9b778a76ab2a226",
+      captureUncaught: true,
+      captureUnhandledRejections: true
+    })
+  );
 
   const throwError = () => {
     throw new Error("react test error");
