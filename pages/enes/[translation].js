36b161530abff3de6227fb13e3073207020b27e2
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-04 02:07:44 -0500

    translating the english text in getStaticProps but the result isnt being passed to the page for some reason

--- pages/enes/[translation].js
+++ pages/enes/[translation].js
@@ -1,23 +1,57 @@
 import { useRouter } from "next/router";
 
-const Enes = () => {
+import LanguageTranslatorV3 from "ibm-watson/language-translator/v3";
+import { IamAuthenticator } from "ibm-watson/auth";
+// import Rollbar from "rollbar";
+import validator from "validator";
+
+const Enes = ({ result }) => {
   const router = useRouter();
   const { translation } = router.query;
+  console.log(result);
 
   if (router.isFallback) {
+    console.log("loading");
     return <div>Loading...</div>;
   }
 
-  return <p>{translation}</p>;
+  return (
+    <p>
+      Before:{translation} After:{result}
+    </p>
+  );
 };
 
 export async function getStaticProps(context) {
-  console.log(context);
-  console.log(context.params);
+  // const rollbar = new Rollbar({
+  //   // accessToken: process.env.ROLLBAR_SERVER_TOKEN,
+  //   captureUncaught: true,
+  //   captureUnhandledRejections: true
+  // });
+  const languageTranslator = new LanguageTranslatorV3({
+    version: "2018-05-01",
+    authenticator: new IamAuthenticator({
+      apikey: process.env.TRANSLATE_KEY
+    }),
+    url: process.env.TRANSLATE_URL,
+    headers: {
+      "X-Watson-Learning-Opt-Out": "true"
+    }
+  });
 
-  return {
-    props: {}
+  const translateParams = {
+    text: context.params.translation,
+    modelId: "en-es"
   };
+
+  if (validator.isEmpty(translateParams.text)) {
+    throw new Error("Please include some text to translate");
+  }
+
+  const translationResult = await languageTranslator.translate(translateParams);
+  const result = await translationResult.result.translations[0].translation;
+
+  return { props: { result } };
 }
 
 export async function getStaticPaths() {
