7fe87eb73c8960cb5ed399b38263b2d43fbcfdbe
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-05 17:05:29 -0500

    database stuff but getting conditional exports warning

--- pages/[id].js
+++ pages/[id].js
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
-import translations from "../translations.json";
+
+import { v4 as uuidv4 } from "uuid";
+import connectToMongo from "../database";
 
 const Post = ({ result }) => {
   const router = useRouter();
@@ -21,24 +23,31 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps(context) {
-  // await new Promise(resolve => setTimeout(resolve, 5000));
-  // console.log(context);
-  // console.log(translations.ex);
-  const result = translations.ex.filter(
-    entry => entry.en === context.params.id
-  );
-  console.log(result);
-  // console.log(result[0].en);
-  // if (result === "hello" || result === "teet") {
-  //   return { props: { result } };
-  // }
-  // if (result) {
-  //   return { props: { result: result[0].en } };
-  // }
+  // const { connection, models } = await connectToMongo();
+  // const { Translations } = models;
 
-  translations.ex.push({ en: context.params.id });
-  console.log(translations.ex);
+  // const docs = await Translations.find();
+  // console.log(docs);
+  // if (docs.length === 0) {
+  //   // if (validator.isEmpty(req.body.comment)) {
+  //   //   throw new Error("Please write something!");
+  //   // }
+  //   console.log("no docs");
+  //   const translation = context.params.id;
+  //   const entry = new Translations({
+  //     preTrans: translation,
+  //     postTrans: translation,
+  //     id: uuidv4(),
+  //     date: new Date()
+  //   });
+  //   await entry.save();
+  //   connection.close();
+  //   return { props: {} };
+  // }
+  console.log("docs found");
 
+  // connection.close();
+  // return { props: { result: docs[0].postTrans } };
   return { props: {} };
 }
 
