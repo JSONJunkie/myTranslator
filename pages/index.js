4ee881218c88884bdfe98e527911fc2dc71666c8
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-05 16:03:30 -0500

    start towards using mongo for translation storage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import { useState, Fragment } from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 import Link from "next/link";
 
@@ -9,28 +10,41 @@ const Index = () => {
     setText(e.target.value);
   };
 
+  function ActiveLink({ children, href, as }) {
+    const router = useRouter();
+    const style = {
+      marginRight: 10,
+      color: router.pathname === href ? "red" : "black"
+    };
+
+    const handleClick = e => {
+      e.preventDefault();
+      router.push(href, as, { shallow: true });
+    };
+
+    return (
+      <a href={href} as={as} onClick={handleClick}>
+        {children}
+      </a>
+    );
+  }
+
   return (
     <div>
-      <div>ssg rendered paths: "test"</div>
-      <div>type "test" for path that doesnt need fallback</div>
-      <div>type anything else for path that does need fallback</div>
       <input name="text" onChange={e => onChange(e)} />
-      <div>
+      <ActiveLink href={"/[id]"} as={"/" + text}>
+        Hellllo
+      </ActiveLink>
+      {/* <div>
         <Link prefetch={false} href="/ex/[id]" as={"/ex/" + text}>
           <a>Correct (client side navigation, page doesnt refresh)</a>
         </Link>
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <Link prefetch={false} href={"/ex/" + text} as={"/ex/" + text}>
           <a>Incorrect(not clientside, page refreshes)</a>
         </Link>
-      </div>
-      <div>
-        client side navigation from index to a non ssg route never shows the
-        fallback. fallback only shows if navigation from index is done
-        incorrectly (and page refreshes) or if the route is typed directly into
-        the address bar as a url from anywhere (new/fresh page loads)
-      </div>
+      </div> */}
     </div>
   );
 };
