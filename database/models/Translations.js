c0bbf862117493803bfdf4441a538efeb2f4d3f2
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-05 17:28:50 -0500

    uncomment database stuff

--- database/models/Translations.js
+++ database/models/Translations.js
@@ -1,22 +1,22 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const TranslationsSchema = new mongoose.Schema({
-//   preTrans: {
-//     type: String,
-//     required: true
-//   },
-//   postTrans: {
-//     type: String,
-//     required: true
-//   },
-//   id: {
-//     type: String,
-//     required: true
-//   },
-//   date: {
-//     type: String,
-//     required: true
-//   }
-// });
+const TranslationsSchema = new mongoose.Schema({
+  preTrans: {
+    type: String,
+    required: true
+  },
+  postTrans: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  }
+});
 
-// export default TranslationsSchema;
+export default TranslationsSchema;
