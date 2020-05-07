d7b4c0866e2a1ced0738830f8d6a4fe7cf48ae86
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-06 21:10:18 -0500

    Api endpoint for getting number of translations

---
+++ pages/api/translations.js
@@ -0,0 +1,22 @@
+import nextConnect from "next-connect";
+
+import { middleware } from "../../database";
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  try {
+    const { Translations } = req.models;
+    const { connection } = req.connection;
+    const numDocs = await Translations.estimatedDocumentCount().exec();
+    res.json(numDocs);
+    connection.close();
+  } catch (err) {
+    res.status(400).json({ errors: { name: err.name, message: err.message } });
+    // rollbar.error(err);
+  }
+});
+
+export default handler;
