cfc7afbaef2dcd30f6005727d176081653b20a4e
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-06 23:48:14 -0500

    Copyright component

---
+++ components/Copyright.js
@@ -0,0 +1,17 @@
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+
+const Copyright = () => {
+  return (
+    <Typography variant="caption text" color="textSecondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://github.com/jsonjunkie">
+        JSONJunkie
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+export default Copyright;
