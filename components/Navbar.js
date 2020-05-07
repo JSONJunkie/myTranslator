d15f7ec1ec1c55cc23805a7cf169b66febca4eed
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-07 03:59:45 -0500

    responsive search/translate bar in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,68 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
 import { Fragment } from "react";
+import InputBase from "@material-ui/core/InputBase";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
+import TranslateIcon from "@material-ui/icons/Translate";
+
+import ButtonLink from "./ButtonLink";
 
 const useStyles = makeStyles(theme => ({
   hidden: {
     visibility: "hidden"
+  },
+  spacing: {
+    marginLeft: theme.spacing(0),
+    [theme.breakpoints.up("md")]: {
+      marginLeft: theme.spacing(2)
+    }
+  },
+  input: {
+    paddingLeft: theme.spacing(1),
+    paddingRight: theme.spacing(13),
+    paddingTop: theme.spacing(0.5),
+    paddingBottom: theme.spacing(0.5),
+    [theme.breakpoints.up("md")]: {
+      paddingTop: theme.spacing(0.5),
+      paddingBottom: theme.spacing(0.5)
+    }
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 1),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.85)
+    },
+    marginTop: theme.spacing(0.5),
+    marginBottom: theme.spacing(0.5),
+    [theme.breakpoints.up("md")]: {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2)
+    }
+  },
+  button: {
+    position: "absolute",
+    right: theme.spacing(0.5),
+    top: theme.spacing(0.7),
+    [theme.breakpoints.up("md")]: {
+      top: theme.spacing(0.5)
+    }
+  },
+  titleDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex"
+    }
+  },
+  titleMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none"
+    }
   }
 }));
 
@@ -16,9 +72,38 @@ const Navbar = () => {
   return (
     <Fragment>
       <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h3">Translator</Typography>
-        </Toolbar>
+        <Container maxWidth="lg" disableGutters={true}>
+          <Toolbar>
+            <Grid container spacing={2} alignItems="center">
+              <Grid item>
+                <Grid container className={classes.titleDesktop}>
+                  <Typography variant="h4">Translator</Typography>
+                </Grid>
+                <Grid container className={classes.titleMobile}>
+                  <TranslateIcon fontSize="large" />
+                </Grid>
+              </Grid>
+              <Grid item xs className={classes.spacing}>
+                <div className={classes.search}>
+                  <InputBase
+                    className={classes.input}
+                    autoFocus
+                    fullWidth
+                    size="small"
+                  />
+                  <ButtonLink
+                    className={classes.button}
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                  >
+                    Translate
+                  </ButtonLink>
+                </div>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </Container>
       </AppBar>
       <Toolbar className={classes.hidden}>
         <Typography variant="h3">Translator</Typography>
