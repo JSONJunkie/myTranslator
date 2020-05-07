d50414f07b97b1c68174b8f38fb35bf114a93cc8
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-06 22:23:02 -0500

    show number of translations in footer instead of navbar

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,13 @@
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
+import { useEffect, useState } from "react";
 import Typography from "@material-ui/core/Typography";
+import { useRouter } from "next/router";
+import Grow from "@material-ui/core/Grow";
+import Container from "@material-ui/core/Container";
+
+import { numOfTranslations } from "../src/actions/translations";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -7,12 +15,47 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const Footer = () => {
+const Footer = ({ numOfTranslations, translations: { numTrans } }) => {
   const classes = useStyles();
+
+  const router = useRouter();
+
+  const [routeChange, setRouteChange] = useState(true);
+  const [grow, setGrow] = useState(false);
+
+  const handleRouteChangeComplete = url => {
+    setRouteChange(prev => true);
+  };
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (routeChange) {
+      numOfTranslations();
+      setRouteChange(prev => false);
+    }
+  }, [routeChange]);
+
+  useEffect(() => {
+    if (numTrans) {
+      setGrow(prev => true);
+    }
+  }, [numTrans]);
 
   return (
     <div className={classes.root}>
-      <Typography variant="h3">Footer</Typography>
+      <Container maxWidth="md">
+        <Grow in={grow} timeout={2000}>
+          <Typography>
+            Total number of translations available: {numTrans}
+          </Typography>
+        </Grow>
+      </Container>
     </div>
   );
 };
@@ -21,4 +64,13 @@ const Footer = () => {
 //   rollbar: PropTypes.object.isRequired
 // };
 
-export default Footer;
+Footer.propTypes = {
+  numOfTranslations: PropTypes.func.isRequired,
+  translations: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  translations: state.translations
+});
+
+export default connect(mapStateToProps, { numOfTranslations })(Footer);
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,61 +1,23 @@
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { useRouter } from "next/router";
-import Grow from "@material-ui/core/Grow";
-
-import { numOfTranslations } from "../src/actions/translations";
 
 const useStyles = makeStyles(theme => ({
   root: {
     marginBottom: theme.spacing(1)
-  },
-  stuff: {
-    marginLeft: "auto"
   }
 }));
 
-const Navbar = ({ numOfTranslations, translations: { numTrans } }) => {
+const Navbar = () => {
   const classes = useStyles();
-
-  const router = useRouter();
-
-  const [routeChange, setRouteChange] = useState(true);
-
-  const handleRouteChangeComplete = url => {
-    setRouteChange(prev => true);
-  };
-
-  useEffect(() => {
-    router.events.on("routeChangeComplete", handleRouteChangeComplete);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChangeComplete);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (routeChange) {
-      numOfTranslations();
-      setRouteChange(prev => false);
-    }
-  }, [routeChange]);
 
   return (
     <Fragment>
       <AppBar position="fixed">
         <Toolbar>
           <Typography variant="h3">Translator</Typography>
-          <Grow in={numTrans}>
-            <span className={classes.stuff}>
-              <Typography>
-                Total number of translations available: {numTrans}
-              </Typography>
-            </span>
-          </Grow>
         </Toolbar>
       </AppBar>
       <Toolbar className={classes.root}>
@@ -69,13 +31,4 @@ const Navbar = ({ numOfTranslations, translations: { numTrans } }) => {
 //   rollbar: PropTypes.object.isRequired
 // };
 
-Navbar.propTypes = {
-  numOfTranslations: PropTypes.func.isRequired,
-  translations: PropTypes.object.isRequired
-};
-
-const mapStateToProps = state => ({
-  translations: state.translations
-});
-
-export default connect(mapStateToProps, { numOfTranslations })(Navbar);
+export default Navbar;
