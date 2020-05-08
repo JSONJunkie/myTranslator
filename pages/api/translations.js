0a4c823bff58110b2c29561d842f55c5cfff2c03
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-08 15:59:28 -0500

    add hit api route

--- pages/api/translations.js
+++ pages/api/translations.js
@@ -18,4 +18,27 @@ handler.get(async (req, res) => {
   }
 });
 
+handler.patch(async (req, res) => {
+  try {
+    const { Translations } = req.models;
+    // console.log(req.body.data.translation);
+    const preTrans = req.body.data.translation;
+    const doc = await Translations.findOne({ preTrans });
+    const updatedDoc = await Translations.findOneAndUpdate(
+      { preTrans },
+      { hits: doc.hits + 1 },
+      { new: true, useFindAndModify: false }
+    );
+    console.log(updatedDoc);
+    res.json(updatedDoc);
+    req.connection.close();
+  } catch (err) {
+    console.log(err);
+    req.connection.close();
+
+    // res.status(400).json({ errors: { name: err.name, message: err.message } });
+    // rollbar.error(err);
+  }
+});
+
 export default handler;
