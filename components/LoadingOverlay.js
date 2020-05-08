482a8d4680d271c6659db0b7161eec109f20f79c
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-08 04:46:16 -0500

    hide loading overlay if no user input, show loading overlay if there is user input

--- components/LoadingOverlay.js
+++ components/LoadingOverlay.js
@@ -1,4 +1,8 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import clsx from "clsx";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
@@ -9,6 +13,14 @@ import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles(theme => ({
   root: {
+    position: "absolute",
+    display: "flex",
+    height: "100%",
+    width: "100%",
+    flexGrow: 1
+  },
+  hideRoot: {
+    visibility: "hidden",
     position: "absolute",
     display: "flex",
     height: "100%",
@@ -48,11 +60,31 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const LoadingOverlay = () => {
+const LoadingOverlay = ({ translations: { userInput } }) => {
   const classes = useStyles();
 
+  const [hide, setHide] = useState(true);
+
+  useEffect(() => {
+    if (userInput) {
+      setHide(prev => false);
+    }
+
+    if (!userInput) {
+      setHide(prev => true);
+    }
+    return () => {
+      setHide(prev => true);
+    };
+  }, [userInput]);
+
+  console.log(userInput);
   return (
-    <div className={classes.root}>
+    <div
+      className={clsx(classes.root, {
+        [classes.hideRoot]: hide
+      })}
+    >
       <Container className={classes.content} maxWidth="md">
         <Grid
           container
@@ -102,4 +134,12 @@ const LoadingOverlay = () => {
 //   rollbar: PropTypes.object.isRequired
 // };
 
-export default LoadingOverlay;
+LoadingOverlay.propTypes = {
+  translations: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  translations: state.translations
+});
+
+export default connect(mapStateToProps)(LoadingOverlay);
