1f9e9d75cc72a8b40ddb50597b8450ceb946da0d
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-08 05:09:38 -0500

    index page design

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,96 @@
 import { makeStyles } from "@material-ui/core/styles";
+import { useEffect, useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import clsx from "clsx";
 import Container from "@material-ui/core/Container";
-import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex"
-  },
-  content: {
+    display: "flex",
+    height: "100%",
     flexGrow: 1
   },
-  paper: {
-    padding: theme.spacing(2),
-    marginTop: theme.spacing(1)
+  content: {
+    display: "flex"
+  },
+  cardRoot: {
+    width: 360
+  },
+  title: {
+    fontSize: 50
+  },
+  translation: {
+    fontSize: 30
+  },
+  pos: {
+    marginBottom: 12
+  },
+  hidden: {
+    fontSize: 30,
+    visibility: "hidden"
   }
 }));
 
-const Index = () => {
+const Index = ({ translations: { userInput } }) => {
   const classes = useStyles();
 
+  const [hide, setHide] = useState(true);
+
+  useEffect(() => {
+    if (userInput) {
+      setHide(prev => false);
+    }
+
+    if (!userInput) {
+      setHide(prev => true);
+    }
+    return () => {
+      setHide(prev => true);
+    };
+  }, [userInput]);
+
   return (
-    <div className={classes.root}>
-      <Container className={classes.content}>
-        <Paper className={classes.paper}></Paper>
+    <div
+      className={clsx(classes.root, {
+        [classes.hideRoot]: hide
+      })}
+    >
+      <Container className={classes.content} maxWidth="md">
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          alignContent="center"
+        >
+          <Card className={classes.cardRoot}>
+            <CardContent>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+              >
+                Welcome!
+              </Typography>
+              <Divider />
+              <Typography className={classes.pos} color="textSecondary">
+                translating from english to spanish...
+              </Typography>
+              <Typography
+                className={classes.translation}
+                variant="body2"
+                component="p"
+              >
+                Your translation will apear here.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
       </Container>
     </div>
   );
@@ -31,4 +100,12 @@ const Index = () => {
 //   rollbar: PropTypes.object.isRequired
 // };
 
-export default Index;
+Index.propTypes = {
+  translations: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  translations: state.translations
+});
+
+export default connect(mapStateToProps)(Index);
