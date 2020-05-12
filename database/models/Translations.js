e51f12a6b1ac3cd7e807db81f9073d78127a300c
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-12 11:27:54 -0500

    new model for translations

--- database/models/Translations.js
+++ database/models/Translations.js
@@ -1,13 +1,81 @@
 import mongoose from "mongoose";
 
 const TranslationsSchema = new mongoose.Schema({
-  preTrans: {
+  ar: {
     type: String,
-    required: true
+    required: false
   },
-  postTrans: {
+  zh: {
     type: String,
-    required: true
+    required: false
+  },
+  "zh - TW": {
+    type: String,
+    required: false
+  },
+  en: {
+    type: String,
+    required: false
+  },
+  fi: {
+    type: String,
+    required: false
+  },
+  fr: {
+    type: String,
+    required: false
+  },
+  de: {
+    type: String,
+    required: false
+  },
+  it: {
+    type: String,
+    required: false
+  },
+  ja: {
+    type: String,
+    required: false
+  },
+  ko: {
+    type: String,
+    required: false
+  },
+  pt: {
+    type: String,
+    required: false
+  },
+  ro: {
+    type: String,
+    required: false
+  },
+  ru: {
+    type: String,
+    required: false
+  },
+  sk: {
+    type: String,
+    required: false
+  },
+  es: {
+    type: String,
+    required: false
+  },
+  sv: {
+    type: String,
+    required: false
+  },
+  th: {
+    type: String,
+    required: false
+  },
+  tr: {
+    type: String,
+    required: false
+  },
+  vi: {
+    type: String,
+    required: false
   },
   hitData: [
     {
