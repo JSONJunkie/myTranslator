98a39efe83200dbc80307ec62b39accfb3b846c2
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-12 20:20:08 -0500

    small typo fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -97,7 +97,7 @@ const Navbar = ({
     router.events.on("routeChangeStart", handleRouteChangeStart);
     router.events.on("routeChangeComplete", handleRouteChangeComplete);
     return () => {
-      router.events.on("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeStart", handleRouteChangeStart);
       router.events.off("routeChangeComplete", handleRouteChangeComplete);
     };
   }, []);
