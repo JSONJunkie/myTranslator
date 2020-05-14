0cfd8ab86ebc3e19f90c62aac62d932ec642cde4
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-14 07:16:42 -0500

    final model for storing audio

--- database/models/Translations.js
+++ database/models/Translations.js
@@ -78,8 +78,82 @@ const TranslationsSchema = new mongoose.Schema({
     required: false
   },
   audio: {
-    type: Buffer,
-    required: false
+    ar: {
+      type: Array,
+      required: false
+    },
+    zh: {
+      type: Array,
+      required: false
+    },
+    "zh - TW": {
+      type: Array,
+      required: false
+    },
+    en: {
+      type: Array,
+      required: false
+    },
+    fi: {
+      type: Array,
+      required: false
+    },
+    fr: {
+      type: Array,
+      required: false
+    },
+    de: {
+      type: Array,
+      required: false
+    },
+    it: {
+      type: Array,
+      required: false
+    },
+    ja: {
+      type: Array,
+      required: false
+    },
+    ko: {
+      type: Array,
+      required: false
+    },
+    pt: {
+      type: Array,
+      required: false
+    },
+    ro: {
+      type: Array,
+      required: false
+    },
+    ru: {
+      type: Array,
+      required: false
+    },
+    sk: {
+      type: Array,
+      required: false
+    },
+    es: {
+      type: Array,
+      required: false
+    },
+    sv: {
+      type: Array,
+      required: false
+    },
+    th: {
+      type: Array,
+      required: false
+    },
+    tr: {
+      type: Array,
+      required: false
+    },
+    vi: {
+      type: Array,
+      required: false
+    }
   },
   hitData: [
     {
