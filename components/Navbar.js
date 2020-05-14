8dfc2f4c43a3530827b3690cc21c1bd27d59a0b7
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-14 01:28:25 -0500

    tooltip for translate button

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,6 +13,7 @@ import IconButton from "@material-ui/core/IconButton";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/router";
 import Button from "@material-ui/core/Button";
+import Tooltip from "@material-ui/core/Tooltip";
 
 import {
   updateInput,
@@ -251,15 +252,17 @@ const Navbar = ({
                         }
                       }}
                     />
-                    <Button
-                      type="submit"
-                      className={classes.button}
-                      variant="contained"
-                      color="primary"
-                      size="small"
-                    >
-                      Translate
-                    </Button>
+                    <Tooltip title="Translate">
+                      <Button
+                        type="submit"
+                        className={classes.button}
+                        variant="contained"
+                        color="primary"
+                        size="small"
+                      >
+                        Translate
+                      </Button>
+                    </Tooltip>
                   </div>
                 </form>
               </Grid>
