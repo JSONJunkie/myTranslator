325852e46464119293533535f11d75d49aa1d541
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-21 09:41:03 -0500

    api route for getting trending translations. filters out en-es welcome translation and returns the top 10 results

---
+++ pages/api/trending.js
@@ -0,0 +1,38 @@
+import nextConnect from "next-connect";
+import LanguageTranslatorV3 from "ibm-watson/language-translator/v3";
+import { IamAuthenticator } from "ibm-watson/auth";
+import axios from "axios";
+import DatauriParser from "datauri/parser";
+
+import { middleware } from "../../database";
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  try {
+    const { Translations } = req.models;
+    const docs = await Translations.find(
+      {},
+      {},
+      {
+        lean: true,
+        sort: { lifetimeHits: -1 },
+        limit: 10
+      }
+    );
+
+    const newDocs = docs.filter(doc => {
+      return doc.en.text !== "welcome";
+    });
+
+    res.json(newDocs);
+    req.connection.close();
+  } catch (err) {
+    res.status(400).json({ errors: { name: err.name, message: err.message } });
+    // rollbar.error(err);
+  }
+});
+
+export default handler;
