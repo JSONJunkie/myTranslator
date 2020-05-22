f217ed48f53fba80d83f649185092ca2d3a90e1a
JSONJunkie <25470092+JSONJunkie@users.noreply.github.com>
2020-05-22 05:22:02 -0500

    trending language selector using implementation in Trending component

--- components/Trending.js
+++ components/Trending.js
@@ -6,16 +6,21 @@ import clsx from "clsx";
 import Grid from "@material-ui/core/Grid";
 import PauseIcon from "@material-ui/icons/Pause";
 import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import TrendingUpIcon from "@material-ui/icons/TrendingUp";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
 import Skeleton from "@material-ui/lab/Skeleton";
 import Fade from "@material-ui/core/Fade";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 import { useRouter } from "next/router";
 
 import TrendingChart from "../components/TrendingChart";
 
-import { getTrending } from "../src/actions/translations";
+import { getTrending, selectTrendingLang } from "../src/actions/translations";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -37,8 +42,19 @@ const useStyles = makeStyles(theme => ({
   },
   trending: {
     position: "absolute",
+    height: theme.spacing(4),
     left: theme.spacing(4),
-    top: theme.spacing(-0.5)
+    top: theme.spacing(-0.5),
+    [theme.breakpoints.up("md")]: {
+      height: theme.spacing(5)
+    },
+    background: theme.palette.background.paper
+  },
+  formControl: {
+    width: theme.spacing(9),
+    [theme.breakpoints.up("md")]: {
+      width: theme.spacing(12)
+    }
   },
   scrollToggle: {
     position: "absolute",
@@ -74,12 +90,30 @@ const useStyles = makeStyles(theme => ({
     top: theme.spacing(-0.5),
     background: theme.palette.background.default
   },
-  trendingText: {
-    padding: theme.spacing(0.75),
-    fontSize: 14,
+  trendingTextDesktop: {
+    display: "none",
+    zIndex: 1,
     [theme.breakpoints.up("md")]: {
-      fontSize: 18,
-      padding: theme.spacing(1)
+      position: "absolute",
+      display: "flex",
+      left: theme.spacing(0),
+      right: theme.spacing(3),
+      paddingTop: theme.spacing(1),
+      paddingLeft: theme.spacing(0.5),
+      height: theme.spacing(5)
+    }
+  },
+  trendingTextMobile: {
+    position: "absolute",
+    height: theme.spacing(4),
+    paddingTop: theme.spacing(0.5),
+    paddingLeft: theme.spacing(1.5),
+    right: theme.spacing(3),
+    left: theme.spacing(0),
+    display: "flex",
+    zIndex: 1,
+    [theme.breakpoints.up("md")]: {
+      display: "none"
     }
   },
   hidden: {
@@ -159,7 +193,11 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const Trending = ({ translations: { trending, preTrans }, getTrending }) => {
+const Trending = ({
+  translations: { trending, preTrans, trendingLang },
+  getTrending,
+  selectTrendingLang
+}) => {
   const classes = useStyles();
 
   const router = useRouter();
@@ -169,6 +207,20 @@ const Trending = ({ translations: { trending, preTrans }, getTrending }) => {
   const [buttonPause, setButtonPause] = useState(false);
 
   const [fade, setFade] = useState(false);
+
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(prev => false);
+  };
+
+  const handleOpen = () => {
+    setOpen(prev => true);
+  };
+
+  const handleChange = e => {
+    selectTrendingLang(e.target.value);
+  };
 
   const handlePause = () => {
     if (!buttonPause) {
@@ -266,11 +318,7 @@ const Trending = ({ translations: { trending, preTrans }, getTrending }) => {
                         >
                           {translation.en.text}
                         </Typography>
-                        <div
-                          square={true}
-                          elevation={0}
-                          className={classes.chart}
-                        >
+                        <div className={classes.chart}>
                           <div className={classes.showChart}>
                             <TrendingChart data={translation.hitData} />
                           </div>
@@ -305,11 +353,7 @@ const Trending = ({ translations: { trending, preTrans }, getTrending }) => {
                         >
                           {translation.en.text}
                         </Typography>
-                        <div
-                          square={true}
-                          elevation={0}
-                          className={classes.chart}
-                        >
+                        <div className={classes.chart}>
                           <div className={classes.showChart}>
                             <TrendingChart data={translation.hitData} />
                           </div>
@@ -320,14 +364,95 @@ const Trending = ({ translations: { trending, preTrans }, getTrending }) => {
                 </Grid>
               </div>
               <div className={classes.trending}>
-                <Paper square={true}>
-                  <Typography
-                    className={classes.trendingText}
-                    color="textSecondary"
-                  >
+                <Paper
+                  square={true}
+                  elevation={0}
+                  className={classes.trendingTextDesktop}
+                >
+                  <Typography noWrap={true} color="textSecondary">
                     Trending
                   </Typography>
                 </Paper>
+                <Paper
+                  square={true}
+                  elevation={0}
+                  className={classes.trendingTextMobile}
+                >
+                  <TrendingUpIcon />
+                </Paper>
+                <FormControl
+                  variant="outlined"
+                  size="small"
+                  className={classes.formControl}
+                >
+                  <Select
+                    labelId="trending language"
+                    id="trending language"
+                    open={open}
+                    onClose={handleClose}
+                    onOpen={handleOpen}
+                    value={trendingLang}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value="ar" disabled={trendingLang === "ar"}>
+                      Arabic
+                    </MenuItem>
+                    <MenuItem value="zh" disabled={trendingLang === "zh"}>
+                      Chinese (simplified)
+                    </MenuItem>
+                    <MenuItem value="zh-TW" disabled={trendingLang === "zh-TW"}>
+                      Chinese (traditional)
+                    </MenuItem>
+                    <MenuItem value="en" disabled={trendingLang === "en"}>
+                      English
+                    </MenuItem>
+                    <MenuItem value="fi" disabled={trendingLang === "fi"}>
+                      Finnish
+                    </MenuItem>
+                    <MenuItem value="fr" disabled={trendingLang === "fr"}>
+                      French
+                    </MenuItem>
+                    <MenuItem value="de" disabled={trendingLang === "de"}>
+                      German
+                    </MenuItem>
+                    <MenuItem value="it" disabled={trendingLang === "it"}>
+                      Italian
+                    </MenuItem>
+                    <MenuItem value="ja" disabled={trendingLang === "ja"}>
+                      Japanese
+                    </MenuItem>
+                    <MenuItem value="ko" disabled={trendingLang === "ko"}>
+                      Korean
+                    </MenuItem>
+                    <MenuItem value="pt" disabled={trendingLang === "pt"}>
+                      Portuguese
+                    </MenuItem>
+                    <MenuItem value="ro" disabled={trendingLang === "ro"}>
+                      Romanian
+                    </MenuItem>
+                    <MenuItem value="ru" disabled={trendingLang === "ru"}>
+                      Russian
+                    </MenuItem>
+                    <MenuItem value="sk" disabled={trendingLang === "sk"}>
+                      Slovak
+                    </MenuItem>
+                    <MenuItem value="es" disabled={trendingLang === "es"}>
+                      Spanish
+                    </MenuItem>
+                    <MenuItem value="sv" disabled={trendingLang === "sv"}>
+                      Swedish
+                    </MenuItem>
+                    <MenuItem value="th" disabled={trendingLang === "th"}>
+                      Thai
+                    </MenuItem>
+                    <MenuItem value="tr" disabled={trendingLang === "tr"}>
+                      Turkish
+                    </MenuItem>
+                    <MenuItem value="vi" disabled={trendingLang === "vi"}>
+                      Vietnamese
+                    </MenuItem>
+                  </Select>
+                </FormControl>
               </div>
               <div className={classes.scrollToggle}>
                 <Paper square={true}>
@@ -383,4 +508,6 @@ const mapStateToProps = state => ({
   translations: state.translations
 });
 
-export default connect(mapStateToProps, { getTrending })(Trending);
+export default connect(mapStateToProps, { getTrending, selectTrendingLang })(
+  Trending
+);
